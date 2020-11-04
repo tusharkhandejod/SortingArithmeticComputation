d054f8c2516433f5b0f442e4eb2bdef940cbd42e
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-04 11:22:53 +0530

    Taking three inputs from user

--- welcome.sh
+++ welcome.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "Welcome to the Sorting Arithmetic Computation program"
+
+read -p "Enter the first value=" a
+read -p "Enter the second value=" b
+read -p "Enter the third value=" c
+
